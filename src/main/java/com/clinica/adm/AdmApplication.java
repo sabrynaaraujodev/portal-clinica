f9b8bad652859471627feb9445a585347f2d402d
Sabryna Araujo <sabrynaaraujodev@gmail.com>
2023-08-13 21:41:26 -0300

    projeto inicial

---
+++ src/main/java/com/clinica/adm/AdmApplication.java
@@ -0,0 +1,13 @@
+package com.clinica.adm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdmApplication.class, args);
+	}
+
+}
